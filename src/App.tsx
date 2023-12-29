cf5f97210ccfc02d37e8aa552ec89328f3b16f85
Syed0208 <36785858+Syed0208@users.noreply.github.com>
2023-12-29 12:11:55 +0530

    passing children in a props

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "Tokyo", "Chennai", "Paris", "Bangalore"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Child <span>Alert</span>
+      </Alert>
     </div>
   );
 }
