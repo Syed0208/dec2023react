eee8f7b81fb241a81f130e3ed61ff1e9aec30bd3
Syed0208 <36785858+Syed0208@users.noreply.github.com>
2024-01-01 20:07:20 +0530

    Reusable Button component exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
-      <Alert>
-        Child <span>Alert</span>
-      </Alert>
+      <Button onClick={() => console.log("Button clicked!!")}>My Button</Button>
     </div>
   );
 }
