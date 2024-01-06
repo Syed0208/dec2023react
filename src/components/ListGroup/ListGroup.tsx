cd150d580d470382f0ad7d84667fcdcefa20ec7f
Syed0208 <36785858+Syed0208@users.noreply.github.com>
2024-01-06 19:35:01 +0530

    CSS in JS example using styled components library

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,20 @@
 import { useState } from "react";
-import styles from "./ListGroup.module.css";
+import "./ListGroup.css";
+import styled from "styled-components";
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0px;
+`;
+
+interface ListItemProps {
+  isActive: boolean;
+}
+
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0px;
+  background: ${(props) => (props.isActive ? "blue" : "none")};
+`;
 
 interface Props {
   items: string[];
@@ -9,20 +24,16 @@ interface Props {
 
 function ListGroup({ items, heading, onSelectItem }: Props) {
   //Hook
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No city added.</p>}
-      <ul className={styles.listGroup}>
+      <List>
         {items.map((item, index) => (
-          <li
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
+          <ListItem
+            isActive={index === selectedIndex}
             key={item}
             onClick={() => {
               setSelectedIndex(index);
@@ -30,9 +41,9 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
             }}
           >
             {item}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
