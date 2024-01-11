02493a568760773f20951dd9f9523eb7e32f65b7
Syed0208 <36785858+Syed0208@users.noreply.github.com>
2024-01-11 19:32:00 +0530

    Issue fixed

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -8,7 +8,7 @@ interface Props {
 const ExpandableText = ({ children, maxChars = 100 }: Props) => {
   const [fulltext, setFullText] = useState(false);
 
-  if (children.length <= maxChars) return <p>children</p>;
+  if (children.length <= maxChars) return <p>{children}</p>;
 
   const summaryText = children.substring(0, maxChars);
   var textToDisplay = summaryText + (!fulltext ? "..." : "");
